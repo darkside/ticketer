a04af72d4c528b3107f35378d514f0d11e8a10ab
Darkside <leonardobighetti@gmail.com>
2011-10-13 00:34:18 -0300

    Fixed migration typo

--- db/migrate/20111013003057_create_tickets.rb
+++ db/migrate/20111013003057_create_tickets.rb
@@ -4,7 +4,7 @@ class CreateTickets < ActiveRecord::Migration
       t.references :user
       t.string :name, :null => false
       t.string :description
-      t.string :status, :null => false, :default => User::STATUSES.first # which would be 'open'
+      t.string :status, :null => false, :default => Ticket::STATUSES.first # which would be 'open'
       t.timestamps
     end
   end
