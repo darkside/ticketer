6047deff4761c9028c075a2eb062cb5d09e4b664
Darkside <leonardobighetti@gmail.com>
2011-10-12 23:14:25 -0300

    Added public/javascripts/application.js to .gitignore as its generated by barista.
    Signed-off-by: Darkside <leonardobighetti@gmail.com>

--- public/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-/* DO NOT MODIFY. This file was compiled Wed, 12 Oct 2011 23:13:22 GMT from
- * /home/darkside/work/simpleminded/ticketer/app/coffeescripts/application.coffee
- */
-
-(function() {
-
-}).call(this);
